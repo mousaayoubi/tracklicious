0e28253a4d59ef96a27dbb2c5b620e25823db613
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-01-16 12:09:09 +0200

    Add jquery ajax results to display on success page

--- public/resources/js/tracklicious_success.js
+++ public/resources/js/tracklicious_success.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    
+    var title = localStorage.getItem("songTitle");
+    console.log(title);
+    var api_key = "c6d6c533633ac3e1bae24c739adbed04";
+    renderTracks();
+    function renderTracks() {
+        console.log("I'm activated");
+    };
+    $.ajax({
+        url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track="+title+"&api_key="+api_key+"&format=json",
+        success: function(result) {
+            console.log(result);
+            var newResult = JSON.stringify(result);
+            $(".ajax_results").append(newResult);
+        }
+    });
 })
