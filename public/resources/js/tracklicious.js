202a6339aa8b12ddbd9c991f01efebd38516e709
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-01-17 17:23:06 +0200

    Update tracklicious.js

--- public/resources/js/tracklicious.js
+++ public/resources/js/tracklicious.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
     $(".btn").click(function() {
+        $(".btn").keypress(function(event){
+            if (event.which == 13) {
+                localStorage.setItem("songTitle", $(".form-control").val());
+                event.preventDefault();
+                window.open("./success.html", "_self");
+            }
+        });
         localStorage.setItem("songTitle", $(".form-control").val());
         event.preventDefault();
         window.open("./success.html", "_self");
