7a10a0f24e62be7c08bfe8ea003c726080228aaf
Mousa Ayoubi <mousaayoubi@gmail.com>
2019-01-20 11:55:15 +0200

    Update format using prettier and eslint

--- public/resources/js/tracklicious.js
+++ public/resources/js/tracklicious.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
-    $(".btn").click(function() {
-        $(".btn").keypress(function(event){
-            if (event.which == 13) {
-                localStorage.setItem("songTitle", $(".form-control").val());
-                event.preventDefault();
-                window.open("./success.html", "_self");
-                return false;
-            }
-        });
+  $(".btn").click(function() {
+    $(".btn").keypress(function(event) {
+      if (event.which == 13) {
         localStorage.setItem("songTitle", $(".form-control").val());
         event.preventDefault();
         window.open("./success.html", "_self");
-    })
-})
+        return false;
+      }
+    });
+    localStorage.setItem("songTitle", $(".form-control").val());
+    event.preventDefault();
+    window.open("./success.html", "_self");
+  });
+});
